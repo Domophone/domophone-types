fc1b56563672ae0787cd5363d3d96f478963b094
Ivan Bakaidov <ivan@aacidov.ru>
2021-06-13 13:05:30 +0300

    add device name

--- lib/Device.d.ts
+++ lib/Device.d.ts
@@ -2,6 +2,7 @@ import { UserId } from "./UserId";
 export interface Device {
     chipId: string;
     owner: UserId | null;
+    name: string | null;
     state: number;
     status: "new" | "work";
 }
--- src/Device.ts
+++ src/Device.ts
@@ -3,6 +3,8 @@ import { UserId } from "./UserId";
 export interface Device {
     chipId: string;
     owner: UserId|null;
+    name: string|null
+    
     state: number;
     status: "new"|"work"
 }
